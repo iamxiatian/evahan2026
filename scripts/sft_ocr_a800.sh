dff82efa38a849ffec6ec4c13e8909a63d596c4e
xiatian <xiat@ruc.edu.cn>
2026-02-07 16:33:12 +0000

    add script

--- scripts/sft_ocr_a800.sh
+++ scripts/sft_ocr_a800.sh
@@ -3,8 +3,8 @@ swift sft \
     --dataset /mnt/public/xiatian/workspace/evahan2026/dataset/train_data/Swift_OCR.jsonl \
     --train_type lora \
     --num_train_epochs 10 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --learning_rate 2e-5 \
     --lora_rank 16 \
     --lora_alpha 32 \
@@ -15,7 +15,7 @@ swift sft \
     --save_steps 50 \
     --save_total_limit 5 \
     --logging_steps 5 \
-    --max_length 8192 \
+    --max_length 1024 \
     --output_dir output \
     --system 'You are a helpful assistant.' \
     --warmup_ratio 0.05 \
